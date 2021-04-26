bb95c05b394993cf1b06599da48fb4a267831e6d
Alessandro <alessandro.rinaudo@gmail.com>
2021-04-26 11:33:54 +0200

    init project

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const fs= require("fs");
-const scoring= require("./scoring/scoring"); 
+// const scoring= require("./scoring/scoring"); 
 
 let code= fs.readFileSync("test.js", "utf8");
-let score= scoring.from(code);
+// let score= scoring.from(code);
 console.log("--------", "Result","--------");
-console.log(score);
+// console.log(score);
 
 
