9b79c67f2b413696e6a33b5737d774b4e95e6c75
Alessandro <alessandro.rinaudo@gmail.com>
2021-04-28 16:53:14 +0200

    clean code

--- scoring/helper.js
+++ scoring/helper.js
@@ -1,4 +1,3 @@
-//const _ = require('lodash');
 exports.syntaxCodeOk = (ast) => {
   return 1
 }
@@ -20,29 +19,19 @@ exports.allVariableAre = (ast) => {
       j++;
     }
   }
-  //console.log(variablesAffectations);
+
   for(i=0;i<variablesDeclarations.length;i++) {
     for(j=0;j<variablesAffectations.length;j++) {
-      if(variablesAffectations[j]=== variablesDeclarations[i]) {
-        // console.log(1,variablesAffectations[j]);
-        variablesAffectations[j] = true;
-      }
-
+      if(variablesAffectations[j]=== variablesDeclarations[i]) 
+        variablesAffectations[j] = true
     }
   }
 
-
-  console.log("affectations");
   for(i=0;i<variablesAffectations.length;i++) {
-    if(variablesAffectations[i]!=true) {
+    if(variablesAffectations[i]!=true) 
       return 0;
-    }
   }
 
-   
-  // console.log("Declarations");
-  // for(i=0;i<variablesDeclarations.length;i++)
-  //   console.log(variablesDeclarations[i]);
 
   return 1
 }
